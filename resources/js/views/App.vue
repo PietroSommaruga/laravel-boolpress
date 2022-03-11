6a5da899ac8aa4321b0542ade5ff6023b67239c2
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-03-11 17:55:18 +0100

    done

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,9 +1,22 @@
 <template>
-    <h1>benvenuto in ve.js</h1>
+    <div>
+        <h1>benvenuto in ve.js</h1>
+        <ul>
+            <li v-for="i in 10" :key="i">{{ i }}</li>
+        </ul>
+        
+        <ExampleComponentVue></ExampleComponentVue>
+    </div>
 </template>
 
 <script>
+import ExampleComponent from '../../../vendor/laravel/ui/src/Presets/vue-stubs/ExampleComponent.vue';
+import ExampleComponentVue from "../components/ExampleComponent.vue";
 export default {
+    components: {
+        ExampleComponentVue,
+        ExampleComponent
+    }
 
 }
 </script>
