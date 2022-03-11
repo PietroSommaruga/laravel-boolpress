7b7efe67573c9df05c7bdabb42ae14224e989ec1
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-03-11 16:03:55 +0100

    create routes group

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,24 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/admin', 'Admin\HomeController@index')->name('admin.home');
+// Route::get('/admin', 'Admin\HomeController@index')->name('admin.home');
+// Route::get('/admin/products', 'Admin\ProductController@index')->name('admin.product.index');
+// Route::get('/admin/products/create', 'Admin\ProductController@index')->name('admin.product.create');
+// Route::get('/admin/products/edit', 'Admin\ProductController@index')->name('admin.product.edit');
+// Route::get('/admin/posts', 'Admin\PostController@index')->name('admin.post.index');
+// Route::get('/admin/posts/create', 'Admin\PostController@index')->name('admin.post.create');
+// Route::get('/admin/posts/edit', 'Admin\PostController@index')->name('admin.post.edit');
+
+Route::middleware("auth")
+->namespace("Admin")
+->prefix("admin")
+->name("admin.")
+->group(function() {
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/products', 'ProductController@index')->name('product.index');
+    Route::get('/products/create', 'ProductController@index')->name('product.create');
+    Route::get('/products/edit', 'ProductController@index')->name('product.edit');
+    Route::get('/posts', 'PostController@index')->name('post.index');
+    Route::get('/posts/create', 'PostController@index')->name('post.create');
+    Route::get('/posts/edit', 'PostController@index')->name('post.edit');
+});
