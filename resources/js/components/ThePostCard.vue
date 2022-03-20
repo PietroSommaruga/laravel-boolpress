6a85709975822ff6df6660069bbf6c89a11c525b
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-03-20 19:10:56 +0100

    done

---
+++ resources/js/components/ThePostCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="col">
+      <div class="card">
+          <div class="card-body">
+              <h5 class="card-title">{{ post.title }}</h5>
+              <p class="card-text" v-html="post.content"></p>
+
+              <em>{{ post.user.name }}; Creato il: {{ post.created_at }}</em>
+              <img :src=" post.image ?  post.image : 'https://www.logistec.com/wp-content/uploads/2017/12/placeholder.png' " alt="" class="index-img">
+          </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: Object,
+  },
+  methods: {
+    
+  },
+};
+</script>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,22 +1,37 @@
 <template>
     <div>
         <TheNavbar></TheNavbar>
-        <ExampleComponentVue></ExampleComponentVue>
+        <!-- <ThePostCard v-for="post of posts" :key="post.id" :post="post"></ThePostCard> -->
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+          <ThePostCard v-for="post of posts" :key="post.id" :post="post"></ThePostCard>
+        </div>
+        
     </div>
 </template>
 
 <script>
 import TheNavbar from "../components/TheNavbar.vue";
-import ExampleComponentVue from "../components/ExampleComponent.vue";
-export default {
-    components: {
-        ExampleComponentVue,
-        TheNavbar
-    }
+import ThePostCard from "../components/ThePostCard.vue";
+import axios from "axios";
 
-}
+export default {
+  components: { TheNavbar, ThePostCard},
+  data() {
+    return {
+      posts: []
+
+    };
+  },
+  methods: {
+    async fetchPosts() {
+      const resp = await axios.get("/api/posts")
+      this.posts = resp.data;
+    }
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
